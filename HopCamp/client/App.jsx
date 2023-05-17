b8006964621582ebb7186c08f7b0efb60b7106d1
DanielPurdy509 <danielpurdy509@gmail.com>
2023-05-17 16:54:13 -0700

    Maps complete, working on saftey partners

--- HopCamp/client/App.jsx
+++ HopCamp/client/App.jsx
@@ -1,5 +1,8 @@
 import { useState } from 'react'
 import "./App.css"
+import Camping_Location from "./components/Camping_Location/Camping_Location.jsx";
+import Tent_Site_Map from './components/Tent_site/Tent_site_map.jsx';
+import Safety_Partners from './components/Safety_Partners/Safety_Partners.jsx';
 
 function App() {
 
@@ -14,13 +17,19 @@ function App() {
     <h1>Current Campsite</h1>
     <h1>Camp Info</h1>
     <h1>Available Sites</h1>
-    <h1>Tent Sites (plus map)</h1>
-    <h1>Camping Location</h1>
+    <div>
+      <Tent_Site_Map />
+    </div>
+    <div>
+      <Camping_Location />
+    </div>
     <h1>Location</h1>
     <h1>Hosted by</h1>
     <h1>Camping Spots</h1>
     <h1>Camp Safety</h1>
-    <h1>Safety & Partners</h1>
+    <div>
+      <Safety_Partners />
+    </div>
     <h1>Footer</h1>
     </>
   )
